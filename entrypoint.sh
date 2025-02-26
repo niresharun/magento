d1ff0d775f4ab01ba8c971a443e2c636ec967ca6
Devops <devops@ziffity.com>
2025-02-26 21:58:40 +0530

    deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Run Magento setup commands
+cd /var/www/html/public
+composer install
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 php bin/magento cache:flush
 
-# Start PHP-FPM in the background
-php-fpm -D
-
-# Start Nginx in the foreground
-nginx -g "daemon off;"
